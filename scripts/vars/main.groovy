9f845e2ab5ea74721fc42480d229dc1a513b708b
ibacangan <i.bacangan@gmail.com>
2022-05-13 01:53:09 +0800

    segregate script functions

--- scripts/vars/main.groovy
+++ scripts/vars/main.groovy
@@ -14,71 +14,15 @@ def initialize() {
                 team.apps.each { app ->
                     def defaultBindings = ["team": team.name, "app": app.name]
                     switch(app.type) {
-                        case "springboot": addSpringbootPipelines(app.name, renderer, defaultBindings)
+                        case "springboot": springboot.addSpringbootPipelines(app.name, renderer, defaultBindings)
                             break
                         default: echo 'type not found'
                     }
                 }
-                addCategorizedViewJobDsl(team.name,
+                utility.addCategorizedViewJobDsl(team.name,
                         "^(${team.apps.collect { it.name }.join('|')})-(build|deploy-to-dev|deploy-to-prod)",
                         "^(.*)-(build|deploy-to-dev|deploy-to-prod)")
             }
         }
     }
-}
-
-def addSpringbootPipelines(String app, Closure renderer, HashMap<String, String> defaultBindings) {
-    addPipelineJobDsl("${app}-build", renderer("springboot.groovy", defaultBindings + ["deployToDevJob": "${app}-deploy-to-dev"]))
-    addPipelineJobDsl("${app}-deploy-to-dev", renderer("argoDeployment.groovy", defaultBindings))
-    addPipelineJobDsl("${app}-deploy-to-prod", renderer("argoDeployment.groovy", defaultBindings))
-}
-
-def addPipelineJobDsl(String jobName, String renderedPipeline) {
-    def scriptTxt = """
-            pipelineJob('$jobName') {
-                definition {
-                    cps {
-                        script('''${renderedPipeline}''')
-                    }
-                }
-            }
-            """
-
-    addJobDsl(scriptTxt)
-}
-
-def addCategorizedViewJobDsl(String viewName, String jobRegex, String regexGroupingRule) {
-    def categorizedViewTxt = """
-                categorizedJobsView(/${viewName}/) {
-                    jobs {
-                        regex('${jobRegex}')
-                        names('')
-                    }
-                    categorizationCriteria {
-                        regexGroupingRule(/${regexGroupingRule}/)
-                    }
-                    columns {
-                        status()
-                        categorizedJob()
-                        lastSuccess()
-                        lastFailure()
-                        lastDuration()
-                        buildButton()
-                    }
-                }
-                """
-
-    addJobDsl(categorizedViewTxt)
-}
-
-def addJobDsl(String scriptTxt) {
-    jobDsl failOnSeedCollision: true,
-            ignoreMissingFiles: false,
-            ignoreExisting: false,
-            removedConfigFilesAction: 'DELETE',
-            removedJobAction: 'DELETE',
-            removedViewAction: 'DELETE',
-            sandbox: false,
-            unstableOnDeprecation: true,
-            scriptText: scriptTxt
 }
---
+++ scripts/vars/utility.groovy
@@ -0,0 +1,84 @@
+def addMultiBranchPipelinePullRequestJobDsl(String jobName, String renderedPipeline) {
+    def scriptTxt = """
+        multibranchPipelineJob('qdqdqd') {
+          configure {
+            it / factory(class: 'org.jenkinsci.plugins.inlinepipeline.InlineDefinitionBranchProjectFactory', plugin: "inline-pipeline@1.0.1") {
+              script('''${renderedPipeline}''')
+              sandbox(true)
+              markerFile('README.md')
+            }
+          }
+          
+          branchSources {
+            branchSource {
+              source {
+                github {
+                  repoOwner('trialstudio')
+                  repositoryUrl('https://github.com/trialstudio/zz.git')
+                  configuredByUrl(true)
+                  repository('zz')
+                  credentialsId('github')
+                  traits {
+                    gitHubBranchDiscovery {
+                      strategyId 2
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+        """
+
+    addJobDsl(scriptTxt)
+}
+
+def addPipelineJobDsl(String jobName, String renderedPipeline) {
+    def scriptTxt = """
+            pipelineJob('$jobName') {
+                definition {
+                    cps {
+                        script('''${renderedPipeline}''')
+                    }
+                }
+            }
+            """
+
+    addJobDsl(scriptTxt)
+}
+
+def addCategorizedViewJobDsl(String viewName, String jobRegex, String regexGroupingRule) {
+    def categorizedViewTxt = """
+                categorizedJobsView(/${viewName}/) {
+                    jobs {
+                        regex('${jobRegex}')
+                        names('')
+                    }
+                    categorizationCriteria {
+                        regexGroupingRule(/${regexGroupingRule}/)
+                    }
+                    columns {
+                        status()
+                        categorizedJob()
+                        lastSuccess()
+                        lastFailure()
+                        lastDuration()
+                        buildButton()
+                    }
+                }
+                """
+
+    addJobDsl(categorizedViewTxt)
+}
+
+def addJobDsl(String scriptTxt) {
+    jobDsl failOnSeedCollision: true,
+            ignoreMissingFiles: false,
+            ignoreExisting: false,
+            removedConfigFilesAction: 'DELETE',
+            removedJobAction: 'DELETE',
+            removedViewAction: 'DELETE',
+            sandbox: false,
+            unstableOnDeprecation: true,
+            scriptText: scriptTxt
+}
